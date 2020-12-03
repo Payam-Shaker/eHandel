a9fac09831e16cee902d81b1a44cd8d8bd068140
Kriistoffer <kjohansson93@hotmail.se>
2020-12-03 19:38:04 +0100

    First commit

---
+++ eHandel/AppManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eHandel
+{
+    class AppManager
+    {
+        public static void DisplayMenu()
+        {
+            Console.Title = "eCommerce Squad";
+            Console.WriteLine("Welcome to eCommerce Squads Online Shop.");
+            Console.WriteLine("");
+            Console.WriteLine("[1]Show all the products.");
+            Console.WriteLine("[2]Show one specific product.");
+            Console.WriteLine("[3]Show the shopping cart.");
+            Console.WriteLine("[4]Add your name and adress to your profile.");
+            Console.WriteLine("[5]Make the order.");
+            Console.WriteLine("[0]Exit the online shop.");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("Pick one option:");
+            string Input = Console.ReadLine();
+
+            if (Input == "1")
+            {
+                //Visa alla produkter
+            }
+            else if (Input == "2")
+            {
+                //Visa en specifik produkt
+            }
+            else if (Input == "3")
+            {
+                //Visa kundvagnen
+            }
+            else if (Input == "4")
+            {
+                //Lägg till namn och adress till din profil före leverans
+                Profile.AddInformation();
+            }
+            else if (Input == "5")
+            {
+                //Gör beställning
+            }
+            else if (Input == "0")
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                /*Detta är en mindre sofistikerad lösning just nu, får se om vi kan göra något bättre här (tänker på .Sleep).
+                 Om du fyller i något som inte är ett alternativ så kommer du få ett meddelande och systemet väntar 5 sekunder
+                innan huvudmenyn återställer sig. .Thread.Sleep gör dock att hela programmet ej kommer att svara under tiden.
+                
+                 Inte särskilt snyggt, men det löser det. Kanske hade varit snyggare med en timer som räknar till t.ex. 5, 
+                ska kika lite på det.*/
+                Console.WriteLine("");
+                Console.WriteLine("You have picked an invalid option, please select an existing one.");
+                System.Threading.Thread.Sleep(5000);
+
+                Console.Clear();
+                AppManager.DisplayMenu();
+            }
+
+        }
+    }
+}
---
+++ eHandel/Profile.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eHandel
+{
+    class Profile
+    {
+
+        private static string firstName;
+        private static string lastName;
+        private static string deliveryAdress;
+
+
+        public static void AddInformation()
+        {
+            /*Följande kod ber kund fylla i förnamn, efternamn, och leveransadress. Värdena sparas i tre privata variabler.*/
+            Console.Clear();
+            Console.WriteLine("Add information to your customer profile.");
+            Console.WriteLine("");
+
+            Console.WriteLine("What is your first name?");
+            SetFirstName(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("What is your last name?");
+            SetLastName(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("What is the delivery adress?");
+            SetDeliveryAdress(Console.ReadLine());
+
+
+            /*Ber kund dubbelkolla den ifyllda informationen. Om den är korrekt fyller kund i "Yes" och kan därefter återgå till
+             huvudmenyn. Fyller kund i "No" (eller valrfritt annat just nu) så får kund fylla i informationen på nytt.*/
+            Console.Clear();
+            Console.WriteLine("You have entered the following: ");
+            Console.WriteLine("First name: " + GetFirstName());
+            Console.WriteLine("Last name: " + GetLastName());
+            Console.WriteLine("Delivery adress: " + GetDeliveryAdress());
+
+            Console.WriteLine("");
+            Console.WriteLine("Is the delivert information correct (Yes or no)?");
+            string input = Console.ReadLine();
+
+            if (input == "Yes")
+            {
+                Console.Clear();
+                AppManager.DisplayMenu();
+            }
+            else
+            {
+                Console.Clear();
+                Profile.AddInformation();
+            }
+        }
+
+
+        /*Getters och setters för de privata variablerna som innehåller namn och leveransadress.*/
+        private static void SetFirstName(string _firstName)
+        {
+            firstName = _firstName;
+        }
+
+        private static void SetLastName(string _lastName)
+        {
+            lastName = _lastName;
+        }
+
+        private static void SetDeliveryAdress(string _deliveryAdress)
+        {
+            deliveryAdress = _deliveryAdress;
+        }
+
+        public static string GetFirstName()
+        {
+            return firstName;
+        }
+
+        public static string GetLastName()
+        {
+            return lastName;
+        }
+
+        public static string GetDeliveryAdress()
+        {
+            return deliveryAdress;
+        }
+    }
+}
